b282ac11de1c7c0258f87f12e856168ccf274ee6
LahariRajanna <141474513+LahariRajanna@users.noreply.github.com>
2023-11-28 23:56:48 +0530

    Delete lab 4 directory

--- lab 4/TicketingSystem.java
+++
@@ -1,9 +0,0 @@
-package railways;
-
-public interface TicketingSystem {
-    void bookTicket(String passengerName, int seatNumber);
-
-    void cancelTicket(int seatNumber);
-
-    void displayTicketDetails(int seatNumber);
-}
